85ee6409ea750888933d88b34bf723172430011b
jianghao <978561120@qq.com>
2026-09-18 15:52:55 +0800

    feat: wire unread dot and notification lifecycle

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,23 @@
 ﻿<script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, watch } from "vue";
 import { useRoute } from "vue-router";
 import ShopTabbar from "./components/ShopTabbar.vue";
 import { useUserStore } from "./stores/user";
+import { useNotificationStore } from "./stores/notification";
 
 const route = useRoute();
 const userStore = useUserStore();
+const notificationStore = useNotificationStore();
 
-onMounted(() => { userStore.validate(); });
+onMounted(() => {
+  userStore.validate();
+  if (userStore.isLoggedIn) notificationStore.connect();
+});
+
+watch(() => userStore.isLoggedIn, (loggedIn) => {
+  if (loggedIn) notificationStore.connect();
+  else notificationStore.disconnect();
+});
 </script>
 
 <template>
--- frontend/src/components/ShopTabbar.vue
+++ frontend/src/components/ShopTabbar.vue
@@ -1,6 +1,8 @@
 ﻿<script setup lang="ts">
 import { useShopCart } from "../stores/shop";
+import { useNotificationStore } from "../stores/notification";
 const { count } = useShopCart();
+const notificationStore = useNotificationStore();
 </script>
 
 <template>
@@ -8,7 +10,7 @@ const { count } = useShopCart();
     <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
     <van-tabbar-item icon="apps-o" to="/category" badge="热">分类</van-tabbar-item>
     <van-tabbar-item icon="shopping-cart-o" to="/cart" :badge="count || undefined">购物车</van-tabbar-item>
-    <van-tabbar-item icon="user-o" to="/profile" dot>我的</van-tabbar-item>
+    <van-tabbar-item icon="user-o" to="/profile" :dot="notificationStore.unread > 0">我的</van-tabbar-item>
   </van-tabbar>
 </template>
 
